efd0469e502f14e34fad793690f4800c80d486a5
Azimjon Pulatov <azimjon@mysuperdispatch.com>
2019-04-10 16:33:59 +0500

    [bot] get_updates view added

--- ustoztop/bot/views.py
+++ ustoztop/bot/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+import telebot
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from .bot import bot
 
-# Create your views here.
+@csrf_exempt
+def get_updates(request):
+    json_string = request.body.decode('utf-8')
+    update = telebot.types.Update.de_json(json_string)
+    bot.process_new_updates([update])
+    return HttpResponse('')
