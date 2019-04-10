8d5e9828b65c0e8be7e1485d4fc457f8e2c98a44
Azimjon Pulatov <azimjon@mysuperdispatch.com>
2019-04-10 16:39:02 +0500

    [bot] bot responses added

---
+++ ustoztop/bot/responses.py
@@ -0,0 +1,243 @@
+WELCOME = "@ustoztopbot ga xush kelibsiz! \nДобро пожаловать!"
+ENTER_LANGUAGE = "Iltimos, tilni tanlang. \nПожалуйста, выберите ваш язык."
+
+UZBEK = "O'zbek 🇺🇿"
+RUSSIAN = "Русский 🇷🇺"
+
+MESSAGES = {
+    "UZ": {
+        "WHO_ARE_YOU": "Kimsiz?",
+        "STUDENT": "O'quvchi 👨‍🎓",
+        "TEACHER": "Ustoz 👩‍🏫",
+        "SUBJECT": "Qaysi fandan?",
+        "SUBJECTS": [
+            "Matematika",
+            "Fizika",
+			"Ingliz tili",
+            "Rus tili",	
+            "Kimyo",
+            "Biologiya",
+            "Ona tili/Adabiyot",
+            "Tarix",
+            "Dasturlash (AT)",
+            "Grafika (AT)",
+            "Koreys tili",
+            "Xitoy tili"
+        ],
+        "CHOOSE_REGION": "Viloyatingizni tanlang.",
+        "CHOOSE_DISTRICT": "Tumaningizni tanlang.",
+        "REGIONS": [
+            {
+                "NAME": "Toshkent shahri",
+                "DISTRICTS": [
+                    "Bektemir tumani", "Chilonzor tumani", "M.Ulug`bek tumani", "Mirobod tumani", "Olmazor tumani", "Sergeli tumani", "Shayxontoxur tumani", "Uchtepa tumani", "Yashnobod tumani", "Yakkasaroy tumani", "Yunusobod tumani"
+                ]
+            },
+            {
+                "NAME": "Andijon viloyati", 
+                "DISTRICTS": [
+                    "Andijon shahri", "Andijon tumani", "Asaka tumani", "Baliqchi tumani", "Bo`z tumani", "Buloqboshi tumani", "Izbosgan tumani", "Jalaquduq tumani", "Marhamat tumani", "Oltinko`l tumani", "Paxtaobod tumani", "Qo`rg`ontepa tumani", "Qorasuv shahri", "Shahrixon tumani", "Ulug`nor tumani", "Xo`jaobod tumani", "Xonabod shahri"
+                ]
+            },
+            {
+                "NAME": "Namangan viloyati", 
+                "DISTRICTS": [
+                    "Chortoq tumani", "Chust tumani", "Kosonsoy tumani", "Mingbuloq tumani", "Namangan shahri", "Namangan tumani", "Norin tumani", "Pop tumani", "To`raqo`rg`on tumani", "Uchqo`rg`on tumani", "Uychi tumani", "Yangiqo`rg`on tumani"
+                ]
+            },
+            {
+                "NAME": "Xorazm viloyati", 
+                "DISTRICTS": [
+                    "Urganch shahri", "Bog`ot tumani", "Gurlan tumani", "Qo`shko`pir tumani", "Shovot tumani", "Urganch tumani", "Xiva tumani", "Xonqa tumani", "Xozarasp tumani", "Yangiariq tumani", "Yangibozor tumani"
+                ]
+            },
+            {
+                "NAME": "Toshkent viloyati", 
+                "DISTRICTS": [
+                    "Angren shahri", "Bekobod tumani", "Bekobod shahri", "Bo`ka tumani", "Bo`stonliq tumani", "Chinoz tumani", "Chirchiq shahri", "O`rtachirchiq tumani", "Ohangaron tumani", "Olmaliq shahri", "Oqqo`rg`on tumani", "Parkent tumani", "Piskent tumani", "Qibray tumani", "Quyichirchiq tumani", "Yangiyo`l tumani", "Yuqorichirchiq tumani", "Zangiota tumani", "Toshkent tumani"
+                ]
+            },
+            {
+                "NAME": "Buxoro viloyati", 
+                "DISTRICTS": [
+                    "Buxoro shahri", "Buxoro tumani", "G`ijduvon tumani", "Jondor tumani", "Kogon shahri", "Kogon tumani", "Olot tumani", "Peshku tumani", "Qorako`l tumani", "Qorovulbozor tumani", "Romitan tumani", "Shofirkon tumani", "Vobkent tumani"
+                ]
+            },
+            {
+                "NAME": "Samarqand viloyati", 
+                "DISTRICTS": [
+                    "Bulung`ur tumani", "Ishtixon tumani", "Jomboy tumani", "Kattaqo`rg`on tumani", "Kottaqo`rg`on shahri", "Narpay tumani", "Nurobod tumani", "Oqdaryo tumani", "Pastdarg`om tumani", "Paxtachi tumani", "Payariq tumani", "Qo`shrabot tumani", "Samarqand shahri", "Samarqand tumani", "Toyloq tumani", "Urgut tumani"
+                ]
+            },
+            {
+                "NAME": "Jizzax viloyati", 
+                "DISTRICTS": [
+                    "Jizzax shahri", "Arnasoy tumani", "Baxmal tumani", "Do`stlik tumani", "Forish tumani", "G`allaorol tuman", "Sharof Rashidov tumani", "Mirzacho`l tumani", "Paxtakor tumani", "Yangiobod tumani", "Zafarobod tumani", "Zarbdor tumani", "Zomin tumani"
+                ]
+            },
+            {
+                "NAME": "Navoiy viloyati", 
+                "DISTRICTS": [
+                    "Karmana tumani", "Konimex tumani", "Navbahor tumani", "Navoiy shahri", "Nurota tumani", "Qiziltepa tumani", "Tomdi tumani", "Uchquduq tumani", "Xatirchi tumani", "Zarafshon shahri"
+                ]
+            },
+            {
+                "NAME": "Qoraqalpog’iston Respublikasi", 
+                "DISTRICTS": [
+                    "Amudaryo tumani", "Beruniy tumani", "Chimboy tumani", "Ellikqal`a tumani", "Kegayli tumani", "Konlikul tumani", "Korauzak tumani", "Kungrod tumani", "Moynok tumani", "Nukus shahri", "Nukus tumani", "Shumanoy tumani", "Taxtakupir tumani", "To`rtko`l tumani", "Xo`jayli tumani"
+                ]
+            },
+            {
+                "NAME": "Sirdaryo viloyati", 
+                "DISTRICTS": [
+                    "Guliston shahri", "Boyovut tumani", "Guliston tumani", "Mirzaobod tumani", "Oqoltin tumani", "Sardoba tumani", "Sayxunobod tumani", "Shirin shahri", "Sirdaryo tumani", "Xovos tumani", "Yangiyer shahri"
+                ]
+            },
+            {
+                "NAME": "Surxondaryo viloyati", 
+                "DISTRICTS": [
+                    "Termiz shahri", "Angor tumani", "Boysun tumani", "Denov tumani", "Jarqo`rg`on tumani", "Muzrabot tumani", "Oltinsoy tumani", "Qiziriq tumani", "Qumqo`rg`on tumani", "Sariosiyo tumani", "Sherobod tumani", "Sho`rchi tumani", "Termiz tumani", "Uzun tumani"
+                ]
+            },
+            {
+                "NAME": "Qashqadaryo viloyati", 
+                "DISTRICTS": [
+                    "Qarshi shahri", "Chiroqchi tumani", "Dehqonobod tumani", "G`uzor tumani", "Kasbi tumani", "Kitob tumani", "Koson tumani", "Mirishkor tumani", "Muborak tumani", "Nishon tumani", "Qamashi tumani", "Qarshi tumani", "Shahrisabz tumani", "Yakkabog` tumani", "Shahrisabz shahri" 
+                ]
+            },
+            {
+                "NAME": "Farg’ona viloyat",
+                "DISTRICTS": [
+                    "Bag`dod tumani", "Beshariq tumani", "Buvayda tumani", "Dang`ara tumani", "Farg`ona shahri", "Farg`ona tumani", "Furqat tumani", "Marg`ilon shahri", "O`zbekiston tumani", "Oltiariq tumani", "Qo`qon shahri", "Qo`shtepa tumani", "Quva tumani", "Quvasoy shahri", "Rishton tumani", "So`x tumani", "Toshloq tumani", "Uchko`prik tumani", "Yozyovon tumani"
+                ]
+            }
+        ],
+        "PRICE": "Tahminiy narxi 💰",
+        "PRICES": [
+            "100 000 so'mgacha",
+            "100 000 - 200 000 so'm",
+            "250 000 so'm",
+            "300 000 so'm va undan baland"
+        ],
+        "PHONE": "Bog'lanish uchun telefon raqamingiz",
+        "FINISHED": "Tabriklaymiz 🎉! Sizning habaringizni qabul qildik. \nAdmistrator ko'rganidan, so'ng @ustoztop kanalida ko'rinadi. Rahmat 😊"
+    },
+
+    "RU": {
+        "WHO_ARE_YOU": "А вы кто?",
+        "STUDENT": "Ученик 👨‍🎓",
+        "TEACHER": "Учитель 👩‍🏫",
+        "SUBJECT": "Ваш предмет?",
+        "SUBJECTS": [
+            "математика",
+            "физика",
+            "английский",
+            "русский",
+            "химия",
+            "биология"
+            "русский язык и литература",
+            "история",
+            "программирование",
+            "графика",
+            "корейский язык",
+            "китайский язык",
+        ],
+        "CHOOSE_REGION": "Выберите свой регион.",
+        "CHOOSE_DISTRICT": "Выберите свой район.",
+        "REGIONS": [
+            {
+                "NAME": "Тошкент шаҳри",
+                "DISTRICTS": [
+                    "Бектемир тумани", "Чилонзор тумани", "М.Улуғбек тумани", "Миробод тумани", "Олмазор тумани", "Сергели тумани", "Шайхонтохур тумани", "Учтепа тумани", "Яшнобод тумани", "Яккасарой тумани", "Юнусобод тумани"
+                ]
+            },
+            {
+                "NAME": "Андижон вилояти", 
+                "DISTRICTS": [
+                    "Андижон шаҳри", "Андижон тумани", "Асака тумани", "Балиқчи тумани", "Бўз тумани", "Булоқбоши тумани", "Избосган тумани", "Жалақудуқ тумани", "Марҳамат тумани", "Олтинкўл тумани", "Пахтаобод тумани", "Қўрғонтепа тумани", "Қорасув шаҳри", "Шаҳрихон тумани", "Улуғнор тумани", "Хўжаобод тумани", "Хонабод шаҳри"
+                ]
+            },
+            {
+                "NAME": "Наманган вилояти", 
+                "DISTRICTS": [
+                    "Чортоқ тумани", "Чуст тумани", "Косонсой тумани", "Мингбулоқ тумани", "Наманган шаҳри", "Наманган тумани", "Норин тумани", "Поп тумани", "Тўрақўрғон тумани", "Учқўрғон тумани", "Уйчи тумани", "Янгиқўрғон тумани"
+                ]
+            },
+            {
+                "NAME": "Хоразм вилояти", 
+                "DISTRICTS": [
+                    "Урганч шаҳри", "Боғот тумани", "Гурлан тумани", "Қўшкўпир тумани", "Шовот тумани", "Урганч тумани", "Хива тумани", "Хонқа тумани", "Хозарасп тумани", "Янгиариқ тумани", "Янгибозор тумани"
+                ]
+            },
+            {
+                "NAME": "Тошкент вилояти", 
+                "DISTRICTS": [
+                    "Ангрен шаҳри", "Бекобод тумани", "Бекобод шаҳри", "Бўка тумани", "Бўстонлиқ тумани", "Чиноз тумани", "Чирчиқ шаҳри", "Ўртачирчиқ тумани", "Оҳангарон тумани", "Олмалиқ шаҳри", "Оққўрғон тумани", "Паркент тумани", "Пискент тумани", "Қибрай тумани", "Қуйичирчиқ тумани", "Янгийўл тумани", "Юқоричирчиқ тумани", "Зангиота тумани", "Тошкент тумани"
+                ]
+            },
+            {
+                "NAME": "Бухоро вилояти", 
+                "DISTRICTS": [
+                    "Бухоро шаҳри", "Бухоро тумани", "Ғиждувон тумани", "Жондор тумани", "Когон шаҳри", "Когон тумани", "Олот тумани", "Пешку тумани", "Қоракўл тумани", "Қоровулбозор тумани", "Ромитан тумани", "Шофиркон тумани", "Вобкент тумани"
+                ]
+            },
+            {
+                "NAME": "Самарқанд вилояти", 
+                "DISTRICTS": [
+                    "Булунғур тумани", "Иштихон тумани", "Жомбой тумани", "Каттақўрғон тумани", "Коттақўрғон шаҳри", "Нарпай тумани", "Нуробод тумани", "Оқдарё тумани", "Пастдарғом тумани", "Пахтачи тумани", "Паяриқ тумани", "Қўшработ тумани", "Самарқанд шаҳри", "Самарқанд тумани", "Тойлоқ тумани", "Ургут тумани"
+                ]
+            },
+            {
+                "NAME": "Жиззах вилояти", 
+                "DISTRICTS": [
+                    "Жиззах шаҳри", "Арнасой тумани", "Бахмал тумани", "Дўстлик тумани", "Фориш тумани", "Ғаллаорол туман", "Шароф Рашидов тумани", "Мирзачўл тумани", "Пахтакор тумани", "Янгиобод тумани", "Зафаробод тумани", "Зарбдор тумани", "Зомин тумани"
+                ]
+            },
+            {
+                "NAME": "Навоий вилояти", 
+                "DISTRICTS": [
+                    "Кармана тумани", "Конимех тумани", "Навбаҳор тумани", "Навоий шаҳри", "Нурота тумани", "Қизилтепа тумани", "Томди тумани", "Учқудуқ тумани", "Хатирчи тумани", "Зарафшон шаҳри"
+                ]
+            },
+            {
+                "NAME": "Қорақалпоғистон Республикаси", 
+                "DISTRICTS": [
+                    "Амударё тумани", "Беруний тумани", "Чимбой тумани", "Eлликқал`а тумани", "Кегайли тумани", "Конликул тумани", "Кораузак тумани", "Кунгрод тумани", "Мойнок тумани", "Нукус шаҳри", "Нукус тумани", "Шуманой тумани", "Тахтакупир тумани", "Тўрткўл тумани", "Хўжайли тумани"
+                ]
+            },
+            {
+                "NAME": "Сирдарё вилояти", 
+                "DISTRICTS": [
+                    "Гулистон шаҳри", "Боёвут тумани", "Гулистон тумани", "Мирзаобод тумани", "Оқолтин тумани", "Сардоба тумани", "Сайхунобод тумани", "Ширин шаҳри", "Сирдарё тумани", "Ховос тумани", "Янгиер шаҳри"
+                ]
+            },
+            {
+                "NAME": "Сурхондарё вилояти", 
+                "DISTRICTS": [
+                    "Термиз шаҳри", "Ангор тумани", "Бойсун тумани", "Денов тумани", "Жарқўрғон тумани", "Музработ тумани", "Олтинсой тумани", "Қизириқ тумани", "Қумқўрғон тумани", "Сариосиё тумани", "Шеробод тумани", "Шўрчи тумани", "Термиз тумани", "Узун тумани"
+                ]
+            },
+            {
+                "NAME": "Қашқадарё вилояти", 
+                "DISTRICTS": [
+                    "Қарши шаҳри", "Чироқчи тумани", "Деҳқонобод тумани", "Ғузор тумани", "Касби тумани", "Китоб тумани", "Косон тумани", "Миришкор тумани", "Муборак тумани", "Нишон тумани", "Қамаши тумани", "Қарши тумани", "Шаҳрисабз тумани", "Яккабоғ тумани", "Шаҳрисабз шаҳри" 
+                ]
+            },
+            {
+                "NAME": "Фарғона вилоят",
+                "DISTRICTS": [
+                    "Бағдод тумани", "Бешариқ тумани", "Бувайда тумани", "Данғара тумани", "Фарғона шаҳри", "Фарғона тумани", "Фурқат тумани", "Марғилон шаҳри", "Ўзбекистон тумани", "Олтиариқ тумани", "Қўқон шаҳри", "Қўштепа тумани", "Қува тумани", "Қувасой шаҳри", "Риштон тумани", "Сўх тумани", "Тошлоқ тумани", "Учкўприк тумани", "Ёзёвон тумани"
+                ]
+            }
+        ],
+        "PRICE": "Примерная цена 💰",
+        "PRICES": [
+            "до 100 000 сўм",
+            "100 000 - 200 000 сўм",
+            "250 000 сўм",
+            "300 000 сўм и больше"
+        ],
+        "PHONE": "Номер телефона для обратной связи",
+        "FINISHED": "Поздравляем 🎉! Ваше сообщение получено. \nАдминистратор проверит это и оно появится на канале @ustoztop. Спасибо 😊"
+    }
+}
