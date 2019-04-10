4e53546b9c93794eb3ceac0c40f189da61c620fe
Azimjon Pulatov <azimjon@mysuperdispatch.com>
2019-04-10 16:40:42 +0500

    [bot] Post model registered to admin

--- ustoztop/bot/admin.py
+++ ustoztop/bot/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+admin.site.site_header = 'UstozTop Administration'
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+
+    def publish(modelAdmin, request, queryset):
+        for post in queryset:
+            post.publish()
+        queryset.update(is_published=True)
+
+
+    publish.short_description = 'Publish to @ustoztop channel'
+
+    list_display = (
+        'id',
+        'type',
+        'subject',
+        'region',
+        'price',
+        'phone',
+        'is_published'
+    )
+    actions = [publish,]
+
