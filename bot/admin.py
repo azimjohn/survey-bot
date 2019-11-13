1fe033bd6a50d6f85ab077e354a39debdd58ea42
Azimjon Pulatov <azimjon@mysuperdispatch.com>
2019-11-13 22:43:08 +0500

    register model to admin

--- bot/admin.py
+++ bot/admin.py
@@ -2,3 +2,9 @@ from django.contrib import admin
 from .models import Respondent
 
 admin.site.site_header = 'SurveyBot Admin'
+
+
+@admin.register(Respondent)
+class RespondentAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "last_name", "username", "step")
+    search_fields = ("first_name", "last_name", "username")
